0ecbe2ea5c3ddb54b09d97c36f74dfd760744671
joseph-grabinger <joseph.grabinger@yahoo.de>
2024-05-25 11:29:02 +0200

    Initial Commit

---
+++ CodingChallenge/CodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallengeApp.swift
+//  CodingChallenge
+//
+//  Created by Joseph Grabinger on 25.05.24.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
