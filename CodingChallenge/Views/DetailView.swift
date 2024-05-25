8d2eda2d6ea1732f244208fb16607f48f2fe934a
joseph-grabinger <joseph.grabinger@yahoo.de>
2024-05-25 17:48:42 +0200

    Refactor DetailView

--- CodingChallenge/Views/DetailView.swift
+++ CodingChallenge/Views/DetailView.swift
@@ -11,14 +11,28 @@ struct DetailView: View {
     @ObservedObject var viewModel: DetailViewModel
     
     var body: some View {
-        VStack() {
+        VStack {
             AsyncImageView(
                 url: URL(string: viewModel.photo.url),
                 size: CGSize(width: 600, height: 600)
             )
             
-            Text(viewModel.photo.title)
-                .padding(.bottom)
+            VStack(alignment: .leading, spacing: 8) {
+                HStack (alignment: .top) {
+                    Text("Title: ")
+                        .foregroundColor(.gray)
+                    Text(viewModel.photo.title)
+                        .lineLimit(nil)
+                }
+                
+                HStack {
+                    Text("Album No.: ")
+                        .foregroundColor(.gray)
+                    Text("\(viewModel.photo.albumId)")
+                        .lineLimit(nil)
+                }
+            }
+            .padding(.horizontal)
             
             Spacer()
         }
